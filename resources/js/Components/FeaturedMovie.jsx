f2cf57253e60c46be560988f930932b79340dc6f
InstaCodex <chandrafajar356@gmail.com>
2024-03-07 16:10:30 +0700

    feat: integration detail movie

--- resources/js/Components/FeaturedMovie.jsx
+++ resources/js/Components/FeaturedMovie.jsx
@@ -53,7 +53,7 @@ export default function FeaturedMovie({
                     </div>
                 </div>
                 <Link
-                    href={`/prototype/movie/${slug}`}
+                    href={`/dashboard/movie/${slug}`}
                     className="inset-0 absolute z-50"
                 ></Link>
             </div>
